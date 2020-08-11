c98bed512ee22644f5fa85428f6c6ec909275a8e
Nelson Wu <neljson.wu@gmail.com>
2020-08-10 20:38:30 -0400

    added comments

--- hello.js
+++ hello.js
@@ -1,2 +1,22 @@
-console.log('hello world')
-console.log('bye')
+//If not set up yet:
+    // git config --global user.name "Your Name"
+   //confirm your github/email is correctly set up/linked
+   // to your curret git: 
+   // git config --global user.email 
+      //will output your Github email if you set up correctly
+
+// cd to working directory/folder --> git init 
+  //mkdir creates a new directory/folder 
+
+//Link Github to already existing local files
+  // + new repo
+  // copy url
+  // git clone url 
+
+// git remote -v
+  //tells you if your current git repo on computer
+  // is hooked up to your github profile 
+
+//git add
+//git status
+//git commit
